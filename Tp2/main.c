176d7e4a615934d4158f7f14ab39422ff373267c
Aurélien Delon <aurelienD@MacBook-Pro-de-Aurelien-3.local>
2013-10-28 09:33:24 -0400

    Initial Commit

---
+++ Tp2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Tp2
+//
+//  Created by Aurélien Delon on 2013-10-28.
+//  Copyright (c) 2013 Aurélien Delon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
